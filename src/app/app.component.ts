25fefcf071acd964de815946f811af741f871c41
fausio luis <luisfausio@gmail.com>
2023-09-10 12:52:43 +0200

    add active style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Navegation } from './model/navagation.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'csi';
+  csI_navagation: Navegation[] = [];
+
+  constructor() {
+
+    this.csI_navagation.push(new Navegation("beneficiary-index", "Beneficiary"));
+    this.csI_navagation.push(new Navegation("household-index", "HouseHold"));
+    this.csI_navagation.push(new Navegation("partner-index", "partner"));
+    this.csI_navagation.push(new Navegation("site-index", "Site"));
+
+  }
+
+
+  onLinkClick(btnName: string): void {
+    console.log("btnClicked", btnName);
+
+    this.csI_navagation.forEach((value) => {
+
+      // clean activeCss
+      value.activeCss ="";
+
+      // add active style
+      if( value.name == btnName){
+        value.activeCss ="background-color: black;";
+      }
+
+    })
+  }
+
+
 }
+
+
+/* <li style="background-color: black;"><a routerLink="/beneficiary-index"> Beneficiary</a></li>
+      <li><a routerLink="/household-index"> HouseHold</a></li>
+      <li><a routerLink="/partner-index"> partner</a></li>
+      <li><a routerLink="/site-index"> Site</a></li>
+ */
---
+++ src/app/model/navagation.model.ts
@@ -0,0 +1,10 @@
+export class Navegation {
+  constructor(
+    public link: string,
+    public name: string,
+    public activeCss?: string ) { }
+}
+
+
+/* <li><a routerLink="/household-index"> HouseHold</a></li>
+ */
